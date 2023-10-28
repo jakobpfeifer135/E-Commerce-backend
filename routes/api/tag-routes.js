f6761981dfd7d26ffb0e8bac4a2a694d3c8f6304
jakobpfeifer135 <138628595+jakobpfeifer135@users.noreply.github.com>
2023-10-27 19:22:17 -0500

    fixed issue with the tag updater

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -37,24 +37,24 @@ router.get('/:id', async (req, res) => {
     }
   });
 
-router.put('/:id', async (req, res) => {
-  try {
-    const tagData = await Tag.update({
-      where: {
-        id: req.params.id,
-      },
-    });
-
-    if (!tagData) {
-      res.status(404).json({ message: 'No tag found with that id!' });
-      return;
+  router.put('/:id', async (req, res) => {
+    try {
+      const tagData = await Tag.update(req.body, {
+        where: {
+          id: req.params.id,
+        },
+      });
+  
+      if (!tagData[0]) {
+        res.status(404).json({ message: 'No tag found with that id!' });
+        return;
+      }
+  
+      res.status(200).json({ message: 'Tag updated successfully' });
+    } catch (err) {
+      res.status(500).json(err);
     }
-
-    res.status(200).json(tagData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+  });
 
 router.delete('/:id', async (req, res) => {
     try {
